05dc2122d840f4e90c2257a4e5c74f92a236eeb8
Åke Forslund <ake.forslund@gmail.com>
2018-10-09 20:36:48 +0200

    Install frotz as part of setup.py
    Will install dfrotz to ~/.pyfrotz as part of the installation

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
 from setuptools import setup
+from setuptools.command.install import install
+import os
+import subprocess
+
+
+def compile_and_install_software():
+    """Use the subprocess module to compile/install the C software."""
+    dest_path = os.path.expanduser('~/.pyfrotz/')
+    if os.path.exists(dest_path + 'dfrotz'):
+        return # binary exists no need to build it
+    elif not os.path.exists(dest_path):
+        os.mkdir(dest_path)
+
+    print("Fetching Frotz")
+    # Git clone
+    frotz_repo = 'https://gitlab.com/DavidGriffith/frotz.git'
+    src_path = '/tmp/frotz'
+    subprocess.check_call('git clone {} {}'.format(frotz_repo, src_path),
+                          shell=True)
+
+    print("Building Frotz")
+    # compile the software
+    subprocess.check_call("make dumb", cwd=src_path, shell=True)
+
+    # install the binary
+    cmd = 'cp {}/dfrotz {}'.format(src_path, dest_path)
+    subprocess.check_call(cmd, cwd=src_path, shell=True)
+
+
+class CustomInstall(install):
+    """Custom handler for the 'install' command."""
+    def run(self):
+        compile_and_install_software()
+        super().run()
+
 
 setup(
     name='pyFrotz',
@@ -8,5 +43,6 @@ setup(
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
-    description='minimal wrapper around frotz'
+    description='minimal wrapper around frotz',
+    cmdclass={'install': CustomInstall}
 )
